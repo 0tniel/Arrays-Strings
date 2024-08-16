f6ad590f4a03bd3ea6329a6e2055e8537a4e960a
0tniel <jhiradotniel@gmail.com>
2024-08-16 23:27:22 +0530

    Add files via upload

---
+++ Display Elements.cpp
@@ -0,0 +1,24 @@
+//Otniel Jhirad
+//ENTC A3
+//23070123069
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int arr[5] = {1, 2, 3, 4, 5};
+    int i;
+    for(i = 0; i <= 4; i++)
+    {
+        cout << arr[i] << "\n";
+    }
+    return 0;
+}
+/*Output:
+1
+2
+3
+4
+5
+*/
+
