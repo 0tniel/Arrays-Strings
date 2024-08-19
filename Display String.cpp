f5997d0cd351f671f4d30e3b00843b7425e74399
0tniel <jhiradotniel@gmail.com>
2024-08-19 18:41:04 +0530

    Add files via upload

---
+++ Display String.cpp
@@ -0,0 +1,14 @@
+// Name: Otniel
+// PRN: 23070123069
+// ENTC A3
+#include <iostream>
+using namespace std;
+
+int main() {
+	char str[]="Otniel";
+	cout<<str;
+    return 0;
+}
+/*Output
+Otniel
+*/
